5a7baca2b1aec65127235426b1bcba669772e4e3
Chobits <chobits@live.cn>
2020-11-20 19:50:39 +0800

    1

--- db-helper-vue/tasks/ftp.js
+++ db-helper-vue/tasks/ftp.js
@@ -1,17 +1,17 @@
-const gulp = require("gulp");
+const gulp = require('gulp');
 
-const { CreateFtpGulp } = require("./utils.js");
+const { CreateFtpGulp } = require('./utils.js');
 
 gulp.task(
-  "ftp",
+  'ftp',
   CreateFtpGulp(
     {
-      host: "server3.hsort.com",
-      user: "ftp_user",
-      password: "123456",
+      host: 'server3.hsort.com',
+      user: 'ftp_user',
+      password: '123456',
       port: 21,
-      parallel: 10
+      parallel: 1,
     },
-    "/www/sql-helper"
+    '/www/sql-helper'
   )
 );
