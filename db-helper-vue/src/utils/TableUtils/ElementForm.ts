61db075b05eb1980d156d74314e3bd02aa3d3d7b
Chao <zhangchao@hsort.com>
2024-08-01 15:08:17 +0800

    add el-col

--- db-helper-vue/src/utils/TableUtils/ElementForm.ts
+++ db-helper-vue/src/utils/TableUtils/ElementForm.ts
@@ -1,14 +1,16 @@
 import { IColumn } from "../../models/Index";
 import { TypeIsNumber, TypeIsDate, TypeIsString } from "../TableUtils";
 
-export default function(table: string, cols: IColumn[]) {
+export default function (table: string, cols: IColumn[]) {
   const temp = cols
     .map(c => {
       const label = c.comments || c.name;
 
-      return `<el-form-item label="${label}" prop="${c.name}">
+      return `<el-col :span="24">
+<el-form-item label="${label}" prop="${c.name}">
 ${GetField(c)}
-</el-form-item>`;
+</el-form-item>
+</el-col>`;
     })
     .join("\n");
 
@@ -22,23 +24,20 @@ function GetField(col: IColumn) {
 }
 
 function GetDateInput(col: IColumn) {
-  return `<el-date-picker type="date" placeholder="选择日期" v-model="item.${
-    col.name
-  }"></el-date-picker>`;
+  return `<el-date-picker type="date" placeholder="选择日期" v-model="item.${col.name
+    }"></el-date-picker>`;
 }
 
 function GetInput(col: IColumn) {
   const placeholder = col.comments || "";
 
-  return `<el-input v-model="item.${
-    col.name
-  }" placeholder="${placeholder}"></el-input>`;
+  return `<el-input v-model="item.${col.name
+    }" placeholder="${placeholder}"></el-input>`;
 }
 
 function GetNumInput(col: IColumn) {
   const label = col.comments || "";
 
-  return `<el-input-number type="number" v-model.number="item.${
-    col.name
-  }" label="${label}"></el-input-number>`;
+  return `<el-input-number type="number" v-model.number="item.${col.name
+    }" label="${label}"></el-input-number>`;
 }
