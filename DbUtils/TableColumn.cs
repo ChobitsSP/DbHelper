7b5baf80648534e29f2901be0368022a97101484
Chao <zhangchao@hsort.com>
2024-09-19 15:04:31 +0800

    ulong

--- DbUtils/TableColumn.cs
+++ DbUtils/TableColumn.cs
@@ -13,7 +13,7 @@ namespace DbUtils
         public string type { get; set; }
         public bool null_able { get; set; }
         public string comments { get; set; }
-        public int? character_maximum_length { get; set; }
+        public ulong? character_maximum_length { get; set; }
         public int? numeric_precision { get; set; }
         public int? numeric_scale { get; set; }
     }
--- DbUtils/Utils/MySqlUtils.cs
+++ DbUtils/Utils/MySqlUtils.cs
@@ -27,7 +27,7 @@ namespace DbUtils.Utils
             public string IS_NULLABLE { get; set; }
             public string COLUMN_TYPE { get; set; }
             public string COLUMN_COMMENT { get; set; }
-            public int? CHARACTER_MAXIMUM_LENGTH { get; set; }
+            public ulong? CHARACTER_MAXIMUM_LENGTH { get; set; }
             public int? NUMERIC_PRECISION { get; set; }
             public int? NUMERIC_SCALE { get; set; }
         }
