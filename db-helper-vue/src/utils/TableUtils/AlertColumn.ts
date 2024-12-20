8161b4096443271d793c6b00a03cbe0272b4d191
Chao <zhangchao@hsort.com>
2024-12-20 11:28:20 +0800

    AlertColumn

--- db-helper-vue/src/utils/TableUtils/AlertColumn.ts
+++ db-helper-vue/src/utils/TableUtils/AlertColumn.ts
@@ -18,20 +18,88 @@ export default function (tableName: string, cols: IColumn[]) {
     list = cols.map(t => GetNpgsqlAlertSql(tableName, t));
   }
   else if (dbType === Data.DbTypeOracle) {
-    // ::todo 之后再做
+    list = cols.map(t => GetOracleAlertSql(tableName, t));
   }
 
   return list.join("\r\n");
 }
 
-function GetMsSqlAlertSql(tableName: string, column: IColumn) {
-  return '';
+function GetMsSqlAlertSql(tableName: string, column: IColumn): string {
+  let sql = `ALTER TABLE ${tableName} ADD ${column.name} ${column.type}`;
+
+  if (column.type.toUpperCase().includes('VARCHAR') && column.character_maximum_length) {
+    sql += `(${column.character_maximum_length})`;
+  }
+
+  if (column.type.toUpperCase().includes('DECIMAL') && column.numeric_precision && column.numeric_scale) {
+    sql += `(${column.numeric_precision},${column.numeric_scale})`;
+  }
+
+  sql += column.null_able ? ' NULL' : ' NOT NULL';
+
+  if (column.comments) {
+    sql += `; EXEC sp_addextendedproperty 'MS_Description', N'${column.comments}', 'SCHEMA', 'dbo', 'TABLE', '${tableName}', 'COLUMN', '${column.name}'`;
+  }
+
+  return sql;
 }
 
-function GetMySqlAlertSql(tableName: string, column: IColumn) {
-  return '';
+function GetMySqlAlertSql(tableName: string, column: IColumn): string {
+  let sql = `ALTER TABLE ${tableName} ADD COLUMN ${column.name} ${column.type}`;
+
+  if (column.type.toUpperCase().includes('VARCHAR') && column.character_maximum_length) {
+    sql += `(${column.character_maximum_length})`;
+  }
+
+  if (column.type.toUpperCase().includes('DECIMAL') && column.numeric_precision && column.numeric_scale) {
+    sql += `(${column.numeric_precision},${column.numeric_scale})`;
+  }
+
+  sql += column.null_able ? ' NULL' : ' NOT NULL';
+
+  if (column.comments) {
+    sql += ` COMMENT '${column.comments}'`;
+  }
+
+  return sql;
 }
 
-function GetNpgsqlAlertSql(tableName: string, column: IColumn) {
-  return '';
+function GetNpgsqlAlertSql(tableName: string, column: IColumn): string {
+  let sql = `ALTER TABLE ${tableName} ADD COLUMN ${column.name} ${column.type}`;
+
+  if (column.type.toUpperCase().includes('VARCHAR') && column.character_maximum_length) {
+    sql += `(${column.character_maximum_length})`;
+  }
+
+  if (column.type.toUpperCase().includes('NUMERIC') && column.numeric_precision && column.numeric_scale) {
+    sql += `(${column.numeric_precision},${column.numeric_scale})`;
+  }
+
+  sql += column.null_able ? ' NULL' : ' NOT NULL';
+
+  if (column.comments) {
+    sql += `; COMMENT ON COLUMN ${tableName}.${column.name} IS '${column.comments}'`;
+  }
+
+  return sql;
+}
+
+function GetOracleAlertSql(tableName: string, column: IColumn): string {
+  let sql = `ALTER TABLE ${tableName} ADD ${column.name} ${column.type}`;
+
+  if (column.type.toUpperCase().includes('VARCHAR2') && column.character_maximum_length) {
+    sql += `(${column.character_maximum_length})`;
+  }
+
+  if (column.type.toUpperCase().includes('NUMBER') && column.numeric_precision && column.numeric_scale) {
+    sql += `(${column.numeric_precision},${column.numeric_scale})`;
+  }
+
+  sql += column.null_able ? ' NULL' : ' NOT NULL';
+
+  if (column.comments) {
+    sql += `; COMMENT ON COLUMN ${tableName}.${column.name} IS '${column.comments}'`;
+  }
+
+  return sql;
 }
