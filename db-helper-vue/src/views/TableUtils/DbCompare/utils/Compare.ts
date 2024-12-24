c29947e98e411825dbe7a77079d4691d17977aa3
Chao <zhangchao@hsort.com>
2024-12-24 18:33:25 +0800

    优化数据库比较逻辑，支持表名和列名不区分大小写

--- db-helper-vue/src/views/TableUtils/DbCompare/utils/Compare.ts
+++ db-helper-vue/src/views/TableUtils/DbCompare/utils/Compare.ts
@@ -1,14 +1,9 @@
 import _ from 'lodash';
 import { IColumn } from '@/models/Index';
 
-/**
- * 获取数据库差异
- * @param columns1 参照库
- * @param columns2 对比库
- */
 export function GetDataBaseDiff(columns1: IColumn[], columns2: IColumn[]) {
-  const tableNames1 = _.uniq(columns1.map(col => col.table));
-  const tableNames2 = _.uniq(columns2.map(col => col.table));
+  const tableNames1 = _.uniq(columns1.map(col => col.table.toLowerCase()));
+  const tableNames2 = _.uniq(columns2.map(col => col.table.toLowerCase()));
 
   // 获取对比库缺少的表
   const missingTables = tableNames1.filter(name => !tableNames2.includes(name));
@@ -16,7 +11,10 @@ export function GetDataBaseDiff(columns1: IColumn[], columns2: IColumn[]) {
   // 对比字段类型不一致
   const typeMismatchColumns = _.chain(columns1)
     .map(col1 => {
-      const col2 = columns2.find(c => c.table === col1.table && c.name === col1.name);
+      const col2 = columns2.find(c => 
+        c.table.toLowerCase() === col1.table.toLowerCase() && 
+        c.name.toLowerCase() === col1.name.toLowerCase()
+      );
       if (!col2 || col1.type === col2.type) return null;
       return {
         ...col1,
@@ -26,9 +24,12 @@ export function GetDataBaseDiff(columns1: IColumn[], columns2: IColumn[]) {
 
   // 对比缺少的列
   const missingColumns = _.chain(columns1)
-    .filter(t => !missingTables.includes(t.table))
+    .filter(t => !missingTables.includes(t.table.toLowerCase()))
     .map(col1 => {
-      const col2 = columns2.find(c => c.table === col1.table && c.name === col1.name);
+      const col2 = columns2.find(c => 
+        c.table.toLowerCase() === col1.table.toLowerCase() && 
+        c.name.toLowerCase() === col1.name.toLowerCase()
+      );
       if (!col2) return col1;
       return null;
     }).filter(t => t != null).value();
