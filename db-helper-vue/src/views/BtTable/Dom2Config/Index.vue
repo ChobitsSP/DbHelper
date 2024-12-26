90aacc867546731cfb09398126fa251cd6915d8d
Chao <zhangchao@hsort.com>
2024-12-26 13:55:41 +0800

    Dom2Config

--- db-helper-vue/src/views/BtTable/Dom2Config/Index.vue
+++ db-helper-vue/src/views/BtTable/Dom2Config/Index.vue
@@ -1,51 +1,56 @@
 <template>
   <div>
-    <el-row>
+    <el-row :gutter="20">
       <el-col :span="12">
         <el-input type="textarea"
                   v-model="input"
                   :rows="20"
+                  placeholder="Paste your HTML here"
                   clearable></el-input>
       </el-col>
       <el-col :span="12">
         <el-input type="textarea"
                   :value="output"
                   :rows="20"
+                  placeholder="Output JSON"
                   readonly></el-input>
       </el-col>
-    </el-row>
-    <el-row>
-      <el-button type="primary"
-                 ref="btn">复制</el-button>
     </el-row>
   </div>
 </template>
 
-<script>
-  import $ from 'jquery';
-  import Clipboard from 'clipboard';
+<script lang="ts">
+  import { defineComponent, ref, computed } from 'vue';
 
-  export default {
-    data() {
-      return {
-        input: ''
-      }
-    },
-    computed: {
-      output() {
-        const dom = $(this.input);
+  export default defineComponent({
+    setup() {
+      const input = ref('');
 
-        const config = Object.assign({}, dom.data());
-        delete config.toggle;
+      function getOutput(html: string) {
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, 'text/html');
+
+        const dom = doc.body.firstChild as HTMLElement;
+
+        const config: Record<string, any> = {};
+        for (let attr of dom.attributes) {
+          if (attr.name !== 'data-toggle') {
+            config[attr.name.replace('data-', '')] = attr.value;
+          }
+        }
 
         const columns = [];
 
-        dom.find('th').each((i, el) => {
-          const col = Object.assign({}, $(el).data());
+        dom.querySelectorAll('th').forEach(el => {
+          const col = {};
+          for (let attr of el.attributes) {
+            col[attr.name.replace('data-', '')] = attr.value;
+          }
 
-          const title = $(el).text();
-          if (title != null)
+          const title = el.textContent;
+          if (title != null) {
             col['title'] = title;
+          }
 
           ['formatter', 'cellStyle'].forEach(key => {
             const val = col[key];
@@ -60,19 +65,49 @@
         config.columns = columns;
         return JSON.stringify(config);
       }
-    },
-    async mounted() {
-      await this.$nextTick();
 
-      this.clipboard = new Clipboard(this.$refs.btn.$el, {
-        text: (trigger) => {
-          return this.output
+      const output = computed(() => {
+        try {
+          return getOutput(input.value);
+        } catch {
+          return '';
         }
-      })
+      });
 
-      this.clipboard.on('success', (e) => {
-        this.$message.success('复制成功');
-      })
-    }
-  }
+      return {
+        input,
+        output,
+      };
+    },
+    // computed: {
+    //   output() {
+    //     const dom = $(this.input);
+
+    //     const config = Object.assign({}, dom.data());
+    //     delete config.toggle;
+
+    //     const columns = [];
+
+    //     dom.find('th').each((i, el) => {
+    //       const col = Object.assign({}, $(el).data());
+
+    //       const title = $(el).text();
+    //       if (title != null)
+    //         col['title'] = title;
+
+    //       ['formatter', 'cellStyle'].forEach(key => {
+    //         const val = col[key];
+    //         if (typeof val === 'string') {
+    //           col[key] = 'MyUtils.' + val;
+    //         }
+    //       });
+
+    //       columns.push(col);
+    //     });
+
+    //     config.columns = columns;
+    //     return JSON.stringify(config);
+    //   }
+    // },
+  });
 </script>
--- db-helper-vue/src/views/BtTable/router.ts
+++ db-helper-vue/src/views/BtTable/router.ts
@@ -1,6 +1,6 @@
 ﻿export default [
   {
-    path: "/BtTable/Dom2Config",
+    path: "BtTable/Dom2Config",
     component: () => import("./Dom2Config/Index.vue"),
     meta: {
       title: "bt-table html转配置"
