019ad388c7a91b02cc9010b651f802939b9cf1da
Chao <zhangchao@hsort.com>
2025-01-07 11:49:32 +0800

    useElementHeight

--- db-helper-vue/src/utils/WindowUtils.ts
+++ db-helper-vue/src/utils/WindowUtils.ts
@@ -42,3 +42,34 @@ export function usePageVisibility() {
   // Return the isActive ref as the result of the hook
   return isActive;
 }
+
+interface ElementHeightOptions {
+  getElement: () => HTMLElement;
+  defaultHeight?: number;
+}
+
+export function useElementHeight(props: ElementHeightOptions) {
+  let resizeObserver: ResizeObserver = null;
+
+  const height = ref(props.defaultHeight || 0);
+
+  onMounted(() => {
+    const dom = props.getElement();
+    if (dom) {
+      resizeObserver = new ResizeObserver(([entry]) => {
+        if (entry) {
+          height.value = entry.contentRect.height;
+        }
+      });
+      resizeObserver.observe(dom);
+    }
+  });
+
+  onUnmounted(() => {
+    resizeObserver?.disconnect();
+  });
+
+  return {
+    height,
+  };
+}
