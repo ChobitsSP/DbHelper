2b670c1ffc7010bca56334203764d8ac7ba9328e
Chao <zhangchao@hsort.com>
2025-01-07 09:53:49 +0800

    add codemirror

---
+++ db-helper-vue/src/views/Database/ExecuteSql/components/SqlInput.vue
@@ -0,0 +1,37 @@
+<template>
+  <div contenteditable="true"
+       ref="div"
+       class="sql-input">
+  </div>
+</template>
+
+<script lang="ts">
+  import { defineComponent, ref, watchEffect } from 'vue';
+  import _ from 'lodash';
+
+  export default defineComponent({
+    props: {
+      value: String,
+    },
+    emits: ['input'],
+    setup(props) {
+      const div = ref<HTMLDivElement>();
+
+      return {
+        div,
+      };
+    },
+  });
+</script>
+
+<style scoped>
+  .sql-input {
+    height: 100%;
+    padding: 10px;
+    font-size: 14px;
+    line-height: 1.5;
+    white-space: pre-wrap;
+    word-wrap: break-word;
+    overflow: auto;
+  }
+</style>
