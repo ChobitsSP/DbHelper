db982026dc350a4b9c1a2ef5a96cc01cd1b1af5a
Chao <zhangchao@hsort.com>
2025-02-14 15:03:11 +0800

    autocompletion

--- db-helper-vue/src/views/Database/ExecuteSql/components/SqlInput.vue
+++ db-helper-vue/src/views/Database/ExecuteSql/components/SqlInput.vue
@@ -13,21 +13,43 @@
 </style>
 
 <script lang="ts">
-  import { defineComponent, ref, onMounted, onUnmounted } from 'vue';
+  import { defineComponent, ref, onMounted, onUnmounted, PropType } from 'vue';
   import _ from 'lodash';
 
   import { EditorView, basicSetup } from 'codemirror';
   import { sql } from '@codemirror/lang-sql';
 
+  import { autocompletion, CompletionContext } from '@codemirror/autocomplete';
+
   export default defineComponent({
     props: {
       value: String,
+      autocompletion: {
+        type: Function as PropType<(str: string) => string[]>,
+        default: () => [],
+      },
     },
     emits: ['input'],
     setup(props, { emit }) {
       const div = ref<HTMLDivElement>();
 
       let view: EditorView;
+
+      function myCompletions(context: CompletionContext) {
+        let word = context.matchBefore(/\w*/);
+        if (word === null) return null;
+        if (word.from === word.to && !context.explicit) return null;
+
+        const options = props.autocompletion(word.text).map(t => ({
+          label: t,
+          apply: () => t,
+        }));
+
+        return {
+          from: word.from,
+          options: options,
+        };
+      }
 
       onMounted(() => {
         view = new EditorView({
@@ -36,6 +58,9 @@
           extensions: [
             basicSetup,
             sql(),
+            autocompletion({
+              override: [myCompletions]
+            }),
             EditorView.updateListener.of((update) => {
               if (update.selectionSet || update.docChanged) {
                 const selections = update.state.selection.ranges;
