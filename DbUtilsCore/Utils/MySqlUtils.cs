99b8c1389c88c8b51a19ffe5f4a7c571b2a300a3
Chao <zhangchao@hsort.com>
2025-02-20 11:37:10 +0800

    支持负数

--- DbUtilsCore/Utils/MySqlUtils.cs
+++ DbUtilsCore/Utils/MySqlUtils.cs
@@ -21,7 +21,7 @@ namespace DbUtilsCore.Utils
             public string IS_NULLABLE { get; set; }
             public string COLUMN_TYPE { get; set; }
             public string COLUMN_COMMENT { get; set; }
-            public ulong? CHARACTER_MAXIMUM_LENGTH { get; set; }
+            public long? CHARACTER_MAXIMUM_LENGTH { get; set; }
             public int? NUMERIC_PRECISION { get; set; }
             public int? NUMERIC_SCALE { get; set; }
             public string table_name { get; set; }
