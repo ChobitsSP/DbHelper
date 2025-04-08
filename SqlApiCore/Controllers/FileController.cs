dc4b6fc5288b43d117aea8a8fa6ee185b4ac6648
Chao <zhangchao@hsort.com>
2025-04-08 15:47:16 +0800

    add upload

---
+++ SqlApiCore/Controllers/FileController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SqlApiCore.Models;
+
+namespace SqlApiCore.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FileController : ControllerBase
+    {
+        public class UploadRequest
+        {
+            public IFormFile File { get; set; }
+        }
+
+        [HttpPost("Upload")]
+        public async Task<ItemResult> Upload(UploadRequest req)
+        {
+            var file = req.File;
+            if (file == null || file.Length == 0)
+            {
+                return new ItemResult("File is empty");
+            }
+
+            var saveDir = Path.Combine(Directory.GetCurrentDirectory(), "uploads");
+            if (!Directory.Exists(saveDir))
+            {
+                Directory.CreateDirectory(saveDir);
+            }
+
+            var filePath = Path.Combine(saveDir, file.FileName);
+            using (var stream = new FileStream(filePath, FileMode.Create))
+            {
+                await file.CopyToAsync(stream);
+            }
+            return new ItemResult();
+        }
+    }
+}
